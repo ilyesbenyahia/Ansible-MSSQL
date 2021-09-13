2db129ec5b10e9963b84754f52405257eb949a69
Ilyes <47536563+ilyesbenyahia@users.noreply.github.com>
2021-09-13 14:39:22 +0200

    Update sql.ps1

--- sql.ps1
+++ sql.ps1
@@ -4,5 +4,4 @@ $Query = 'SELECT TOP (1000) [id], [nom], [prenom], [age]  FROM [user].[dbo].[Tab
 $Username = "sa"
 $Password = "Azertyuiop123"
 $sqlCmd = Invoke-SQLCmd -ServerInstance $Server -Database $Database -ConnectionTimeout 300 -QueryTimeout 600 -Query $Query -Username $Username -Password $Password 
-$res = Select-Object $sqlCmd -ExcludeProperty ItemArray, Table, RowError, RowState, HasErrors 
-$res ConvertTo-Json
+Select-Object $sqlCmd -ExcludeProperty ItemArray, Table, RowError, RowState, HasErrors | ConvertTo-Json
