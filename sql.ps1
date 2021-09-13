cbe8b7a1194e2b75bf5607870f46c7eabf7b2ad8
Ilyes <47536563+ilyesbenyahia@users.noreply.github.com>
2021-09-13 10:53:46 +0200

    Update sql.ps1

--- sql.ps1
+++ sql.ps1
@@ -1,1 +1,6 @@
-Invoke-Sqlcmd -Query "SELECT TOP (1000) [id], [nom], [prenom], [age]  FROM [user].[dbo].[Table_1]" -ServerInstance "WS-SQL\SQLEXPRESS" | Select-Object * -ExcludeProperty ItemArray, Table, RowError, RowState, HasErrors | ConvertTo-Json
+$Server = '192.168.1.101\SQLEXPRESS'
+$Database = 'user'
+$Query = 'SELECT TOP (1000) [id], [nom], [prenom], [age]  FROM [user].[dbo].[Table_1]'
+$Username = 'LAB-SI\Administrateur'
+$Password = 'Azertyuiop123'
+Invoke-SQLCmd -ServerInstance $Server -Database $Database -ConnectionTimeout 300 -QueryTimeout 600 -Query $Query -Username $Username -Password $Password | Select-Object * -ExcludeProperty ItemArray, Table, RowError, RowState, HasErrors | ConvertTo-Json
