dec2f4cf014b293c9c797c1713f74797e452531d
Ilyes <47536563+ilyesbenyahia@users.noreply.github.com>
2021-09-13 11:31:25 +0200

    Update sql.ps1

--- sql.ps1
+++ sql.ps1
@@ -1,6 +1,22 @@
-$Server = '192.168.1.101\SQLEXPRESS'
-$Database = 'user'
-$Query = 'SELECT TOP (1000) [id], [nom], [prenom], [age]  FROM [user].[dbo].[Table_1]'
-$Username = 'LAB-SI\Administrateur'
-$Password = 'Azertyuiop123'
-Invoke-SQLCmd -ServerInstance $Server -Database $Database -ConnectionTimeout 300 -QueryTimeout 600 -Query $Query -Username $Username -Password $Password | Select-Object * -ExcludeProperty ItemArray, Table, RowError, RowState, HasErrors | ConvertTo-Json
+
+
+
+$SqlServer = "WS-SQL\SQLEXPRESS"
+$SqlDbname = "user"
+$SqlQuery = $("SELECT TOP (1000) [id], [nom], [prenom], [age]  FROM [user].[dbo].[Table_1]")
+
+
+$SqlConnection =New-Object System.Data.SqlClient.SqlConnection
+$SqlConnection.ConnectionString= "server= $Sqlserver; Database= $sqldbname; Integrated Security =True"
+
+$SqlCmd= New-Object System.Data.sqlClient.SqlCommand
+$SqlCmd.commandText = $SqlQuery
+$SqlCmd.Connection = $SqlConnection
+
+$SqlAdapter=New-Object System.Data.SqlClient.SqlDataAdapter
+$SqlAdapter.SelectCommand = $SqlCmd
+
+$DataSet= New-Object System.Data.DataSet
+$SqlAdapter.Fill($DataSet)
+
+$SqlConnection.Close()
